759e905c012858c13b684c2220768134ea60a714
Vitali <vitali.clicev@gmail.com>
2021-08-31 10:29:39 +0300

    lesson3

--- test.js
+++ test.js
@@ -1,6 +1,38 @@
-let num = 50;
+/* let num = 50; */
 /* while (num < 55) {
     console.log(num);
     num++;
 } */
 
+/* let num = 20;
+
+function showFirst(text){
+    alert(text);
+    num = 10;
+}
+
+showFirst("Hello!");
+console.log(num); */
+
+/* function calc(a,b) {
+    return (a + b);
+}
+
+console.log(calc(3,4));
+
+console.log(calc(8,4)); */
+
+/* function retVar() {
+    let num = 50;
+    return num;
+}
+
+let anotherNum = retVar();
+console.log(anotherNum); */
+
+/* let str = "test";
+console.log(str.length); */
+
+function ucFirst(str) {
+    
+}
